31fb100b7f0d3aa4a0f1ae27189e1262ee691db7
Leah Tait <leah.tait@gmail.com>
2019-04-27 14:11:35 -0700

    adding new js file

--- app.js
+++ app.js
@@ -1,10 +1,75 @@
-var APIkey = "493d5df0-b8e4-46c6-9a73-aad7be18f4ab" ;
-   if (searchStartYear === '' && searchEndYear === '') {
-      queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&facet=true&fl=&q=" + searchItem + "&api-key=" + APIkey;
-    } else if(searchStartYear !== '' && searchEndYear === '') {
-      queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + begin_date + "&facet=true&fl=&q=" + searchItem + "&api-key=" + APIkey;
-    } else if(searchStartYear === '' && searchEndYear != '') {
-      queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?end_date=" + searchEndYear + "&facet=true&fl=&q=" + searchItem + "&api-key=" + APIkey;
-    } else {
-      queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + searchStartYear + "&end_date=" + searchEndYear + "&facet=true&fl=&q=" + searchItem + "&api-key=" + APIkey;
-    }
+$(document).ready (function () {
+    
+    
+    
+    
+    //id for search 
+    input = searchItem
+    
+    //id for number of results 
+    display = numberDisplay
+    
+    //id for search year 
+    start = searchStartYear
+    
+    // id for search year 
+    end = searchEndYear
+    
+    //id for button 
+    submit = submit
+    
+    
+    $("#submit").on("click", function() {
+        //search term
+        var q = $("#searchItem").text();
+        console.log("this is q" +  q)
+        
+        //id for search 
+        // input = searchItem
+        
+        //id for number of results 
+        // display = numberDisplay
+        
+        //id for search year 
+        // start = searchStartYear
+        
+        // id for search year 
+        // end = searchEndYear
+        
+        //id for button 
+        // submit = submit
+        
+        var begin_date =""
+        var end_date=""
+        var queryURL =''
+        
+        // API key
+        APIkey = "mihrp9G14YWEh0F3YWb9WaMyyhoppJZC";
+        // Replaced with if statement
+        //queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + begin_date + "&end_date=" + end_date + "&facet=true&q="+ q +"&api-key=" + APIkey
+        
+        // Code from Catherine goes here
+        if (begin_date === '' && end_date === '') {
+            queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&facet=true&fl=&q=" + q + "&api-key=" + APIkey;
+        } else if(begin_date !== '' && end_date === '') {
+            queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + begin_date + "&facet=true&fl=&q=" + q + "&api-key=" + APIkey;
+        } else if(begin_date === '' && end_date != '') {
+            queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?end_date=" + end_date + "&facet=true&fl=&q=" + q + "&api-key=" + APIkey;
+        } else {
+            queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + begin_date + "&end_date=" + end_date + "&facet=true&fl=&q=" + q + "&api-key=" + APIkey;
+        }
+        
+        //query URL with facets
+        $.ajax({
+            url:queryURL, 
+            method: "GET"
+        }).then(function(response){
+            $(".apiDump").text(JSON.stringify(response));
+            console.log(response)
+            console.log(queryURL);
+            // 
+            
+        }); // ends on click function
+    });
+    
+}); // end the document ready function 
